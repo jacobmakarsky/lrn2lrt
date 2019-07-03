4b67003a1cb770f4dd6c1e5bb0bb72c0bc1822cd
Jacob Makarsky <jmm7293@psu.edu>
2019-07-03 12:48:14 +0800

    just alot of comments entered really

--- contracts/lrnTOlrt.sol
+++ contracts/lrnTOlrt.sol
@@ -4,7 +4,8 @@ pragma solidity >=0.4.0 <0.7.0;
 import "./ECMath.sol";
 import "./EllipticCurve.sol";
 
-contract ERC20 {
+contract ERC20 
+{
 	function totalSupply() public view returns (uint256);
     function balanceOf(address who) public view returns (uint256);
     function transfer(address to, uint256 value) public returns (bool);
@@ -18,52 +19,32 @@ contract ERC20 {
  */
 contract lrnTOlrt
 {
-	address owner; //the owner must be sender of the message
-
-	ERC20 lrt;
-	ECMath math;
-	EllipticCurve ec;
+	ERC20 lrt; //erc20 token object called lrt to mock lrt
+	ECMath math; //ecmath object 
+	EllipticCurve ec; //elliptic curve object
 
 	constructor(address _lrtAddress) public
 	{
-		owner = msg.sender;
 		//Define LRT as an ERC20 token
 		lrt = ERC20(_lrtAddress);
+
 		math = new ECMath();
 		ec = new EllipticCurve();
 	}
 
-	//require owner of neoAddress to be the sender
-	modifier onlyOwner
-    {
-        require (msg.sender == owner, "Not owner of contract");
-        _;
-    }
-
 	//Mapping from NEO-address to number of claimable LRT tokens
 	mapping (string => uint) neoBalances;
 
-	function setNeoBalance(string memory neoAddr, uint balance) public onlyOwner
+	function setNeoBalance(string memory neoAddr, uint balance) public
 	{
 		neoBalances[neoAddr] = balance;
 	}
 
-	//Send the coin to the tronAddress only if the sender's NEO signature is valid
-	function claim(address payable tronAddr, string memory neoAddr, bytes memory neoSig) public
-	{
-		require(verifyClaim(tronAddr, neoSig), "Signature invalid");
-		require(neoBalances[neoAddr] > 0, "No tokens to claim");
-		//If all the above conditions are met, then the transfer happen
-    	require(lrt.transfer(tronAddr, neoBalances[neoAddr]), "Claim failed");
-		//The claimers collectable balance is reset after the transfer occurs
-		neoBalances[neoAddr] = 0;
-	}
-
 	//Verifies the signature of the claimer
-	//Notes: Doesn't seem to distingish between who submits the signature
-	//Seems to accept any valid signature, message doesn't seem to matter
-	//Also requires signature to be in bytes format when calling function (ie add 0x to the front)
-	//A bit more consistent if you sign with a hex rather than text
+	//*******BUGS*******
+	//neo signature and address verification working
+	//tron address and neo signature verification not working
+	//... verification is working when neo signature has any message (should be the matching tron address)
 	function verifyClaim(address tronAddr, bytes memory neoSig) public returns (bool)
 	{
 		bytes memory tronBytes = abi.encodePacked(tronAddr);
@@ -74,16 +55,32 @@ contract lrnTOlrt
 		uint256 s;
 		uint8 v;
 
+		//this gets r,s and v from the signature
 		assembly {
 			r := byte(0, mload(add(neoSig, 32)))
             s := byte(0, mload(add(neoSig, 64)))
             v := byte(0, mload(add(neoSig, 96)))
 		}
 
+		//recover the public key coordinates from the signature
+		//(signature = e, public key = v, r from signature, s from signature)
 		uint256[2] memory pubCoordinates = math.recover(e, v, r, s);
 
 		uint256[2] memory signature = [ r, s ];
 
+		//make sure the signature matches this address and pbkey coordinates
 		return ec.validateSignature(tron, signature, pubCoordinates);
+	}
+
+	//Send the coin to the tronAddress only if the sender's NEO signature is valid
+	function claim(address tronAddr, string memory neoAddr, bytes memory neoSig) public
+	{
+		require(verifyClaim(tronAddr, neoSig), "Signature invalid");
+		require(neoBalances[neoAddr] > 0, "No tokens to claim");
+		//If all the above conditions are met, then the transfer happen
+    	require(lrt.transfer(tronAddr, neoBalances[neoAddr]), "Claim failed");
+
+		//The claimers collectable balance is reset after the transfer occurs for retesting purposes
+		neoBalances[neoAddr] = 0;
 	}
 }
--- test/lrnTOlrtTest.js
+++ test/lrnTOlrtTest.js
@@ -1,42 +1,53 @@
+//*******************************************
+//disperse the mock LRT using lrn2lrt.sol and mock wallets
+
 var l2l = artifacts.require("lrnTOlrt");
 var lrt = artifacts.require("lrt.sol");
 const truffleAssert = require('truffle-assertions');
-//Transfer a token into the test
 
 contract('lrnTOlrt', function(accounts)
 {
-    it("Should claim 32 LRT", async function() {
-        //Deploy necessary contracts
-        let instance = await l2l.deployed();
+    it("Should claim 32 LRT", async function() 
+    {
+         //Deploy necessary contracts and make objects
+        let lrn2lrt = await l2l.deployed();
         let lrtToken = await lrt.deployed();
 
-        const balBefore = await lrtToken.balanceOf("0x3132e27d840b8C1e24a5eE1f1BF611dD6f6f0006");
+        //get tron address balance before transfer
+        const balBefore = await lrtToken.balanceOf("0x632E114f253F89BFb0C31d949320878CBbc73185");
+        
+        //transfer lrt from LRT.sol to lrnTOlrt.sol
         const transferAmount = 32;
+        lrtToken.transfer(lrn2lrt.address, transferAmount);
 
-        //Transfer LRT to be claimed from the conversion contract
-        lrtToken.transfer(instance.address, transferAmount);
+        //set neo address from neo gui and fake address balance
+        await lrn2lrt.setNeoBalance("APgRk6ytaWhsiurSryu9Ks3Ub2Dqkbf8HU", transferAmount);
 
-        //Set LRN balance of a dummy NEO account
-        await instance.setNeoBalance("AJaq6fXGw2MmqkF1zFL5YpESazKUZzpjp4", transferAmount);
-
+        //*****************
+        //(tron address, neo address, neo signature)
+        //(any tron address, neo address matching signature from neo gui, neo signature made with any message in neo gui)
         //Claim the LRN balance of this account in LRT
-        await instance.claim("0x3132e27d840b8C1e24a5eE1f1BF611dD6f6f0006",
-                            "AJaq6fXGw2MmqkF1zFL5YpESazKUZzpjp4",
-                            "0x08c434fea49c3c211b13688fa7d584fb005262ffecdf5eb0186f979893a8b3adf1e852ad219cac7d71326217f2e17c735180ad0888f3463f102b964d7328809e");
+        //neo sig needs 0x added to it when made on neo gui using text
+        await lrn2lrt.claim("0x632E114f253F89BFb0C31d949320878CBbc73185",
+                            "APgRk6ytaWhsiurSryu9Ks3Ub2Dqkbf8HU",
+                            "0x6be40cb81695fdcd3fcf62aab80c0a30eb4d2ba591e18839a1bfa3817f597bdf4cacb891f0eed90ca31cc10c2a355b31b8c7277c5b5c5aa8ac8c6723fa29e9d5");
 
-        let balAfter = await lrtToken.balanceOf("0x3132e27d840b8C1e24a5eE1f1BF611dD6f6f0006");
-        let balDiff = balAfter.sub(balBefore);
+        //get tron address balance after transfer
+        let balAfter = await lrtToken.balanceOf("0x632E114f253F89BFb0C31d949320878CBbc73185");
+        //balDiff = balAfter - balBefore, should = 32
+        let balDiff = (balAfter - balBefore);
 
         //Check if the correct amount was added to the Tron wallet
         assert.equal(transferAmount, balDiff, "32 wasn't added to the account");
     });
 
-    it("Should not claim 32 LRT due to incorrect signature", async function() {
+    it("Should not claim 32 LRT due to incorrect signature", async function() 
+    {
         //Deploy necessary contracts
         let instance = await l2l.deployed();
         let lrtToken = await lrt.deployed();
 
-        const balBefore = await lrtToken.balanceOf("0x3132e27d840b8C1e24a5eE1f1BF611dD6f6f0006");
+        const balBefore = await lrtToken.balanceOf("0x632E114f253F89BFb0C31d949320878CBbc73185");
         const transferAmount = 32;
 
         //Transfer LRT to be claimed from the conversion contract
@@ -46,32 +57,33 @@ contract('lrnTOlrt', function(accounts)
         await instance.setNeoBalance("AJaq6fXGw2MmqkF1zFL5YpESazKUZzpjp4", transferAmount);
 
         //Checks if invalid signature error occurs when calling an incorrect signature
-        await truffleAssert.reverts(instance.claim("0x3132e27d840b8C1e24a5eE1f1BF611dD6f6f0006",
-                                                    "AJaq6fXGw2MmqkF1zFL5YpESazKUZzpjp4",
-                                                    "0x9315439f874e5d698e4caaf2b0a25efe3d23e5c96d1ff7fdb43edbc4c6be8f145e5815e2ac905a1a1565ff685ead1784808f68a395b13728a12098172df7a927"),
+        await truffleAssert.reverts(instance.claim("0x632E114f253F89BFb0C31d949320878CBbc73185",
+                                                    "APgRk6ytaWhsiurSryu9Ks3Ub2Dqkbf8HU",
+                                                    "0x9315439f874e5d698e4caaf2b0a25efe3d23e5c96d1ff7fdb43edbc4c6be8f145e5815e2ac905a1a1565ff685ead1784808f68a395b13728a12098172df7a928"),
                                     "Signature invalid");
     });
 
-    it("Can't claim twice", async function() {
+    it("Can't claim twice", async function() 
+    {
         //Deploy necessary contracts
         let instance = await l2l.deployed();
         let lrtToken = await lrt.deployed();
 
-        const transferAmount = 100;
+        const transferAmount = 32;
 
         //Transfer LRT to be claimed from the conversion contract
         lrtToken.transfer(instance.address, transferAmount);
-        await instance.setNeoBalance("AJaq6fXGw2MmqkF1zFL5YpESazKUZzpjp4", transferAmount);
+        await instance.setNeoBalance("0x632E114f253F89BFb0C31d949320878CBbc73185", transferAmount);
 
         //Makes valid claim for the first time
-        await instance.claim("0x3132e27d840b8C1e24a5eE1f1BF611dD6f6f0006",
-                             "AJaq6fXGw2MmqkF1zFL5YpESazKUZzpjp4",
+        await instance.claim("0x632E114f253F89BFb0C31d949320878CBbc73185",
+                             "APgRk6ytaWhsiurSryu9Ks3Ub2Dqkbf8HU",
                              "0x2323a29ac2b95a439c5b93f356718cda2e3dfd2ad4d12fc4d0b451f068b657a03b35feeca82dbb588bdb8ab0da1aacfa010686030fd24cfb40e0186e0fb8e935");
 
         //Makes valid claim for the second time, checks to see if it's allowed
-        await truffleAssert.reverts(instance.claim("0x3132e27d840b8C1e24a5eE1f1BF611dD6f6f0006",
-                                                    "AJaq6fXGw2MmqkF1zFL5YpESazKUZzpjp4",
-                                                    "0x2323a29ac2b95a439c5b93f356718cda2e3dfd2ad4d12fc4d0b451f068b657a03b35feeca82dbb588bdb8ab0da1aacfa010686030fd24cfb40e0186e0fb8e935"),
+        await truffleAssert.reverts(instance.claim("0x632E114f253F89BFb0C31d949320878CBbc73185",
+                                                    "APgRk6ytaWhsiurSryu9Ks3Ub2Dqkbf8HU",
+                                                    "0x6be40cb81695fdcd3fcf62aab80c0a30eb4d2ba591e18839a1bfa3817f597bdf4cacb891f0eed90ca31cc10c2a355b31b8c7277c5b5c5aa8ac8c6723fa29e9d5"),
                                     "No tokens to claim");
     });
 });
